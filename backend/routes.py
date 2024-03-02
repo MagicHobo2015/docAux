8291774c4ee5c35007a20ad91437b9b48e14319c
Joshua Land <joshua.land6@gmail.com>
2024-03-02 11:35:04 -0800

    Not many changes just updating the branch cause we have class today, most work has been done to backend database connection

--- backend/routes.py
+++ backend/routes.py
@@ -46,10 +46,10 @@ def main():
     db.init_app(docAux_api)
 
 
-
     if DEBUG:
         with docAux_api.app_context():
             try:
+                db.create_all()
                 # just to check if anything will work.
                 db.session.execute(text('SELECT 1'))
                 print('Connection successful !')
@@ -66,6 +66,41 @@ def main():
     def home():
         pass
 
+<<<<<<< 1f99f6140ab5d7f471584613f37573f94912195b
+=======
+    # we can talk about what the actual urls end up being.
+    @docAux_api.route('/create_user', methods=['POST'])
+    def create_user():
+        data = request.get_json()
+        username = data.get('username')
+        email = data.get('eamil')
+
+        # need these to create a user, there is more just not yet.
+        if not username or not email:               
+                                # TODO: make these error codes mean something
+            return jsonify({'message': 'Username and email are required'}), 400
+        
+        # Now make sure that the user doesnt already exist.
+        user_check = User.query.filter_by(username=username).first()
+        if user_check:
+                                # TODO: make these error codes mean something
+            return jsonify({'message': 'Username already exists'}), 400
+        
+        email_check = User.query.filter_by(email=email).first()
+        if email_check:
+            return jsonify({'message': 'User With That Email Adress Already\
+                             exists'}), 400
+        
+        # here we have everything confirmed, its time to add the user
+        new_user = User(username=username, email=email)
+        db.session.add(new_user)
+        db.session.commit()
+        
+        message = f'User {username} with Email address {email}, Created\
+            succesfully!'
+        return jsonify({'message': message})
+
+>>>>>>> Not many changes just updating the branch cause we have class today, most work has been done to backend database connection
 
 if __name__ == '__main__':
     main()
